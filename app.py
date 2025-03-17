3721afff1f73e318020989d52e63efaa18c7dd95
davidroche7 <davidroche1979@gmail.com>
2025-03-17 19:31:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import random
 app = Flask(__name__)
 
 # Random song title generator
-animals = ["Hamster", "Cat", "Pug", "Penguin", "Llama", "Sloth", "Eleanor", "Weasel", "Bird"]
+animals = ["Charlotte", "Mummy","Eleanor"]
 actions = ["Dancing", "Singing", "Riding a Skateboard", "Eating Tacos", "Twirling"]
 objects = ["Banana", "Cheeseburger", "Spaceship", "Cupcake", "Rainbow", "Unicycle", "Segway"]
 exclamations = ["!", "!!", "!!!"]
